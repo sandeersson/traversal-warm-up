430c5b704ffa1fe40e9788b6e614aa8536fd4add
sandeersson <sandra.anderstrom@gmail.com>
2025-05-28 10:39:17 -0700

    Finished warm up

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -30,6 +30,13 @@ public class TraversalPractice {
      * @param node the root of the tree
      */
     public static void printLeafNodes(TreeNode node) {
+        if (node == null) return;
 
+        if (node.left == null && node.right == null) {
+            System.out.println(node.data);
+        }
+
+        printLeafNodes(node.left);
+        printLeafNodes(node.right);
     }
 }
